acbb113f625ca25d509b55527ce7836626da249c
Duc Quy <nguyenducquy098@gmail.com>
2023-03-09 09:05:22 +0700

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times')
+        $('.navbar').toggleClass('nav-toggle')
+    })
+
+    $('#share').click(function(){
+        $(this).toggleClass('share-active')
+    })
+
+    $(window).on('load scroll', function(){
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        $('section').each(function(){
+            let height = $(this).height();
+            let top =$(window).scrollTop();
+            let id = $(this).attr('id');
+            let offset = $(this).offset().top - 200;
+
+            if(top >= offset && top < height + offset){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+        })
+    })
+
+    $('.screen-slider').owlCarousel({
+        loop:true,
+        center:true,
+        autoplay:true,
+        nav:false,
+        dots:false,
+        responsive:{
+            0:{
+                item:1
+            },
+            710:{
+                item:2
+            },
+            1200:{
+                item:3
+            }
+        }
+    })
+
+    $('.review-slider').owlCarousel({
+        loop:true,
+        center:true,
+        autoplay:true,
+        nav:false,
+        dots:false,
+        responsive:{
+            0:{
+                item:1
+            },
+            710:{
+                item:2
+            },
+            1200:{
+                item:3
+            }
+        }
+    })
+
+})
